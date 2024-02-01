1401721bcda3936b55453ff86cf3f71e099cf8d9
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-01 13:32:55 -0500

    portfolio 1

---
+++ src/portfolioPage.jsx
@@ -0,0 +1,17 @@
+
+const Profile = () => {
+
+    return(
+        <>
+            <h1 className="Name">
+                Theodore Thibodeau
+            </h1>
+
+            <h2 className="aboutMe">
+                About Me
+            </h2>
+        </>
+    );
+};
+
+export default Profile;
