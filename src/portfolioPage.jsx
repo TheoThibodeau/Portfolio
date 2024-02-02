7b69d70668d17db4bb2db83dc6840f845ef6fa54
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-02 15:35:17 -0500

    added an About Me section

--- src/portfolioPage.jsx
+++ src/portfolioPage.jsx
@@ -2,15 +2,56 @@
 const Profile = () => {
 
     return(
-        <>
-            <h1 className="Name">
+        <div className="mainContainer">
+            <h1 className="name">
                 Theodore Thibodeau
             </h1>
 
             <h2 className="aboutMe">
                 About Me
             </h2>
-        </>
+
+            <h3>
+                Hello! My name is Theodore Thibodeau, I am a full-stack software engineer!
+                After a prosperous career working in sensory deprivation float
+                spas, I am making a big leap into the world of software developement. 
+            </h3>
+
+            <h3>
+                I am a huge history buff, I love playing pickleball with my friends, and 
+                spending time with my family.
+            </h3>
+
+            <h3>
+                Fun Facts!
+            </h3>
+
+            <ul className="funFactsList" style={{ textAlign: "left" }}>
+            <li>
+                My first car was a 1968 Volkswagen Beetle that my Dad and I restored 
+                and I still enjoy working on it in my spare time!
+                <svg id="more-arrows">
+                    <polygon class="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
+                    <polygon class="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
+                    <polygon class="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
+                </svg>
+            </li>
+
+
+            <li>
+                I was once an extra in the television show 'Eastbound and Down'.
+            </li>
+        
+            <li>
+                My wife is an Archaeologist and gets to travel around the World 
+                and work on really awesome digs! She is currently in Saudi Arabia 
+                working on the 'Line' Project!! 
+            </li>
+            <a href="https://www.youtube.com/shorts/K_0UCUwhk64">
+                What is the 'Line'?
+            </a>
+            </ul>
+        </div>
     );
 };
 
