694853e4ba6ceb2ff38ef5895b47c399dfbf8a12
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-03 13:39:36 -0500

    HELLO

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Profile from './portfolioPage'
+import { useState } from 'react';
+import './App.css';
+import Profile from './portfolioPage';
+import Welcome from './welcomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Profile />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Welcome />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+    </Routes> 
+    </BrowserRouter>
     </>
   )
 }
